08104126475f58cc88d6c5091a305c27b7e87e5a
zhanshenzhi2008 <lj2010vip@163.com>
2018-10-06 20:47:17 +0800

    修改单元测试目录结构 添加mock单元测试

---
+++ src/main/java/com/orjrs/spring/test/web/TestController.java
@@ -0,0 +1,19 @@
+package com.orjrs.spring.test.web;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TestController
+ *
+ * @author orjrs
+ * @date 2018-10-06 20:10
+ */
+@RestController
+public class TestController {
+    @PostMapping("unit/sayHello/{name}/{message}")
+    public String sayHello(@PathVariable(value = "name") String name, @PathVariable(value = "message") String message) {
+        return name + ": " + message;
+    }
+}
---
+++ src/test/java/com/orjrs/spring/test/unit/MockTest.java
@@ -0,0 +1,48 @@
+package com.orjrs.spring.test.unit;
+
+import com.orjrs.spring.test.TestApplicationTests;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * MockTest
+ *
+ * @author orjrs
+ * @date 2018-10-06 20:15
+ */
+@WebAppConfiguration // 1
+public class MockTest extends TestApplicationTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void post() throws Exception {
+        String name = "Smart LIU";
+        String msg = "您好";
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("unit/sayHello/" + name + "/" + msg)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                //.content()
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
